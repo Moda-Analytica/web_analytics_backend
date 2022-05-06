bed23e9841916b29f722f3d7893f31dbe8951b4b
moyosore1 <moyosoreolumideobi@gmail.com>
2022-05-06 05:34:41 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio
 
 
@@ -6,6 +7,20 @@ from app.config import get_settings
 from app.stats.router import router as SectorRouter
 
 app = FastAPI()
+
+origins = [
+    "https://moda-analytica.netlify.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(SectorRouter, tags=["Sector"], prefix="/sector")
 settings = get_settings()
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_url)
