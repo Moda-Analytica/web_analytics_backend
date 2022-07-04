77a3ef67e61da93af28400188da9d16e15b02a34
moyosore1 <moyosoreolumideobi@gmail.com>
2022-07-04 12:43:46 -0400

    Update utils.py

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,7 +1,37 @@
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from starlette.responses import JSONResponse
+import json
+
+from pywebpush import webpush, WebPushException
 
 
-def create_aliased_response(model: BaseModel) -> JSONResponse:
-    return JSONResponse(content=jsonable_encoder(model, by_alias=True))
+from ..config import get_settings
+
+
+
+
+settings = get_settings()
+
+
+def trigger_push_notification(push_subscription, title, body):
+    try:
+        response = webpush(
+            subscription_info=json.loads(
+                push_subscription.get('subscription_json')),
+            data=json.dumps({"title": title, "body": body}),
+            vapid_private_key=settings.vapid_private_key,
+            vapid_claims={
+                "sub": "mailto:{}".format(settings.vapid_claim_email)
+            }
+        )
+        return response.ok
+    except WebPushException as ex:
+        if ex.response and ex.response.json():
+            extra = ex.response.json()
+            print("Remote service replied with a {}:{}, {}",
+                  extra.code,
+                  extra.errno,
+                  extra.message)
+        return False
+
+
+def trigger_push_notifications_for_subscriptions(subscriptions, title, body):
+    return [trigger_push_notification(subscription, title, body) for subscription in subscriptions]
